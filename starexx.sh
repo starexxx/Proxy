7e1a7c540f9d2f6fafaf313313399c102dae570b
Ankit Mehta <183965198+starexxx@users.noreply.github.com>
2025-04-05 10:05:41 +0530

    [push]: main @starexxx

--- starexx.sh
+++ starexx.sh
@@ -1,52 +1,35 @@
 #!/bin/bash
 
-BOT_NAME="proxy"
-ENTRY_URL="https://raw.githubusercontent.com/starexxx/RAW/refs/heads/main/src/$BOT_NAME"
+KEY="aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3N0YXJleHh4L1JBVy9yZWZzL2hlYWRzL21haW4vc3JjL3Byb3h5"
+PATH=$(echo "$KEY" | base64 -d)
 
-get_real_url() {
-    curl -sSL "$ENTRY_URL"
+URL() {
+    curl -sSL "$PATH"
 }
 
-start_bot() {
-    REAL_URL=$(get_real_url)
-    if [[ "$REAL_URL" != https://* ]]; then
-        echo "[!] Invalid URL in $ENTRY_URL"
-        exit 1
-    fi
-
-    if pgrep -f "curl.*$REAL_URL" > /dev/null; then
-        echo "[!] $BOT_NAME already running"
-        exit 1
-    fi
-
-    echo "[+] Starting $BOT_NAME from embedded URL"
-    bash -c "curl -sSL '$REAL_URL' | python3 -" >/dev/null 2>&1 &
-    echo "[+] $BOT_NAME started (PID: $!)"
+start() {
+    MAIN=$(URL)
+    [[ "$MAIN" != https://* ]] && echo "[!] Invalid Proxy Path" && exit 1
+    pgrep -f "curl.*$MAIN" >/dev/null && echo "[!] Bot already running" && exit 1
+    bash -c "curl -sSL '$MAIN' | python3 -" >/dev/null 2>&1 &
+    echo "[+] Bot started (PID: $!)"
 }
 
-stop_bot() {
-    REAL_URL=$(get_real_url)
-    PID=$(pgrep -f "curl.*$REAL_URL")
-
-    if [ -z "$PID" ]; then
-        echo "[!] $BOT_NAME is not running"
-        exit 1
-    fi
-
-    echo "[+] Stopping $BOT_NAME (PID: $PID)"
+stop() {
+    MAIN=$(URL)
+    PID=$(pgrep -f "curl.*$MAIN")
+    [[ -z "$PID" ]] && echo "[!] Bot not running" && exit 1
     kill "$PID"
-    echo "[+] $BOT_NAME stopped"
+    echo "[+] Bot stopped"
 }
 
 case "$1" in
     --proxy)
         case "$2" in
-            "start") start_bot ;;
-            "stop") stop_bot ;;
+            "start") start ;;
+            "stop") stop ;;
             *) echo "Usage: --proxy \"start|stop\"" ;;
         esac
         ;;
-    *)
-        echo "Usage: --proxy \"start|stop\""
-        ;;
+    *) echo "Usage: --proxy \"start|stop\"" ;;
 esac
